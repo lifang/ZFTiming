c814ba8d64bd3006e581da19734fc16f60196366
lonkicz <caozhen19891217@126.com>
2015-04-29 10:41:55 +0800

    payChannelId修正

--- src/main/java/com/comdosoft/financial/timing/service/ThirdPartyService.java
+++ src/main/java/com/comdosoft/financial/timing/service/ThirdPartyService.java
@@ -64,7 +64,7 @@ public class ThirdPartyService {
 	public String syncStatus(Integer terminalId) throws ServiceException{
 		Terminal terminal = checkTerminal(terminalId);
 		Integer payChannelId = terminal.getPayChannelId();
-		if(payChannelId>5){
+		if(payChannelId>7){
 			return null;
 		}
 		JointManager manager = switchManager(payChannelId);
