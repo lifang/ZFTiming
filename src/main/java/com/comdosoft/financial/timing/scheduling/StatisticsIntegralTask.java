ecee14f904576dbe238b1254af471c27c3398ff8
harold <hudong@epalmpay.cn>
2015-05-21 09:14:16 +0800

    计算分润统计时间修改为：凌晨0点 积分统计时间修改为：凌晨1点

--- src/main/java/com/comdosoft/financial/timing/scheduling/StatisticsIntegralTask.java
+++ src/main/java/com/comdosoft/financial/timing/scheduling/StatisticsIntegralTask.java
@@ -21,7 +21,7 @@ public class StatisticsIntegralTask {
 	/**
 	 * 统计购买的pos机 所获得的积分。
 	 */
-	@Scheduled(cron="0 0 0/2 * * ?")
+	@Scheduled(cron="0 0 1 * * ?")
 	public void statisticsIntegral() {
 		new Thread(new Runnable() {
 			@Override
@@ -41,7 +41,7 @@ public class StatisticsIntegralTask {
 	/**
 	 * 统计交易流水
 	 */
-	@Scheduled(cron="0 0 0/2 * * ?")
+	@Scheduled(cron="0 0 1 * * ?")
 	public void transactionFlowing() {
 		new Thread(new Runnable() {
 			@Override
